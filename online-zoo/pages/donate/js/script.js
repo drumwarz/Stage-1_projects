aa0078ccd572c257a11dbaa8c2e91cdc514aef55
Ihar Sapeha <maddavoice@gmail.com>
2022-10-04 02:56:39 +0300

    feat: add donate adaptive

--- online-zoo/pages/donate/js/script.js
+++ online-zoo/pages/donate/js/script.js
@@ -1,11 +1,16 @@
-// Ограничение символов в input типа number
+const btnBurger = document.querySelector('.header__burger'),
+      burger = document.querySelector('.menu__body'),
+      overlay = document.querySelector('.overlay')
 
-const input = document.querySelector('input[type=number]');
-input.addEventListener('input', () => {
-        if (input.value.length > 4) { input.value = input.value.substr(0, 4) }
-    });
+btnBurger.addEventListener('click', () => {
+    burger.classList.add('menu__body_active');
+    overlay.classList.add('overlay_active');
+})
+overlay.addEventListener('click', () => {
+    burger.classList.remove('menu__body_active');
+    overlay.classList.remove('overlay_active');
+})
 
-// Добавление интерактивности в прогресс-бар
 
 const dot = document.querySelectorAll('.pay__circle');
 const dotActive = document.querySelectorAll('.pay__dot');
