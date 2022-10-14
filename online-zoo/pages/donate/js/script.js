bd97cb88f6aff6a6b5df970032052852f05583cc
Ihar Sapeha <maddavoice@gmail.com>
2022-10-14 21:45:14 +0300

    fix: donate burger menu close

--- online-zoo/pages/donate/js/script.js
+++ online-zoo/pages/donate/js/script.js
@@ -1,7 +1,7 @@
 const btnBurger = document.querySelector('.header__burger'),
       burger = document.querySelector('.menu__body'),
-      overlay = document.querySelector('.overlay')
-
+      overlay = document.querySelector('.overlay'),
+      close = document.querySelector('.menu__close');
 btnBurger.addEventListener('click', () => {
     burger.classList.add('menu__body_active');
     overlay.classList.add('overlay_active');
@@ -11,17 +11,25 @@ overlay.addEventListener('click', () => {
     overlay.classList.remove('overlay_active');
 })
 
+close.addEventListener('click', () => {
+    burger.classList.remove('menu__body_active');
+    overlay.classList.remove('overlay_active');
+})
 
 
 
-// Ограничение символов в input типа number
+
+
 
 const input = document.querySelector('input[type=number]');
 input.addEventListener('input', () => {
         if (input.value.length > 4) { input.value = input.value.substr(0, 4) }
     });
 
-// Добавление интерактивности в прогресс-бар
+
+    
+
+    
 
 const dot = document.querySelectorAll('.pay__circle');
 const dotActive = document.querySelectorAll('.pay__dot');
